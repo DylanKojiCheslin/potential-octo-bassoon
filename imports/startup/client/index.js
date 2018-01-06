0c5d17dfc3435b8bd85629b7b63cc1749c206af9
dylan <dylan.kojicheslin@gmail.com>
2018-01-06 08:52:35 -0700

    bump

---
+++ imports/startup/client/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Meteor} from 'meteor/meteor';
+import {render} from 'react-dom';
+
+const App = () => <h1> Hello </h1>;
+
+
+Meteor.startup( () => {
+  render( <App />, document.getElementById('app'));
+});
